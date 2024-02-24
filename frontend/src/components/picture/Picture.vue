8dab90056ec1874bf3f565212d89cf5e0a292f30
eva <evaorange73@gmail.com>
2024-02-24 15:40:38 +0300

    points

--- frontend/src/components/picture/Picture.vue
+++ frontend/src/components/picture/Picture.vue
@@ -3,17 +3,19 @@
     <rect fill="white" width="300" height="300"/>
     <Figure v-bind:radius="$props.r"/>
     <Axes/>
+    <Points :points="points" :r="r"/>
   </svg>
 </template>
 
 <script>
 import Axes from "@/components/picture/Axes.vue";
 import Figure from "@/components/picture/Figure.vue";
+import Points from "@/components/picture/Points.vue";
 
 export default {
   name: 'Picture',
   props: ['points', 'r'],
-  components: { Axes, Figure }
+  components: { Axes, Figure, Points }
 }
 
 </script>
---
+++ frontend/src/components/picture/Points.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg>
+    <circle v-for="point in computed_points"
+            r="2"
+            :fill="point.color"
+            :cx="point.x"
+            :cy="point.y"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Points',
+  props: ['points', 'r'],
+  computed: {
+    computed_points() {
+      let computed = [];
+      for (let point of this.points) {
+        if(point.r === this.r) {
+          computed.push({
+            x: point.x * 30,
+            y: point.y * 30,
+            color: point.result ? 'green' : 'red'
+          })
+        }
+      }
+      return computed;
+    }
+  }
+}
+</script>
